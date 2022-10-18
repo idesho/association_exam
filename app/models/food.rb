94c8b0defe0dc861ee6275e1587f037bfdbe4831
井出晶 <badminton.1110@i.softbank.jp>
2022-10-18 17:40:55 +0900

    テスト送信

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,8 @@
 class Food < ApplicationRecord
+  has_many :order_foods
+  has_many :orders, through: :order_foods
+  has_one :japanese_food
+  has_one :italian_food
+  has_one :chinese_food
+  belongs_to :shop 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+    belongs_to :food
+    belongs_to :order
 end
