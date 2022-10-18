a03914b0fa496d3757ac45551c30e2402e15a660
井出晶 <badminton.1110@i.softbank.jp>
2022-10-18 17:48:22 +0900

    全て完成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
-    belongs_to :food
-    belongs_to :order
+belongs_to :customer
+  belongs_to :address
+  has_many :order_foods, dependent: :destroy
+  has_many :foods, through: :order_foods, dependent: :destroy
 end
